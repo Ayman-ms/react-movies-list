6155e5a64d9f5e6148ef2386e125c085ab250239
Mohamad Ayman Mslmani <104221357+Ayman-ms@users.noreply.github.com>
2023-01-28 16:15:18 +0100

    Update api.js

--- src/redux/types/api.js
+++ src/redux/types/api.js
@@ -1,5 +1,3 @@
 import { apiKey } from "./apiKey"
 
 export const api='https://api.themoviedb.org/3/movie/popular?api_key='+apiKey+'&language=en-US'
-
-// export const searchApi=`https://api.themoviedb.org/3/search/movie?api_key=`+apiKey+`&query=${filmToSearch}&language=en-US`
